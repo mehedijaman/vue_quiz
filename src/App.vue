197dd7b64ca330ea0745c10aa07815ed6bd9eab1
Mehedi Jaman <mail4mjaman@gmail.com>
2023-07-23 22:49:52 +0600

    - First commit,  Project setup

---
+++ src/App.vue
@@ -0,0 +1,74 @@
+<script setup>
+  import Question from './components/Question.vue'
+
+  import {ref,reactive} from 'vue'
+
+  const questions = [
+    {
+      question:'What is capital of Bangladesh?',
+      options:[
+        {
+          text:'Dhaka',
+          status:true
+        },
+        {
+          text:'Khulna',
+          status:false
+        },
+        {
+          text:'Barishal',
+          status:false
+        },
+        {
+          text:'Chittagonj',
+          status:false
+        },
+      ]
+    },
+    {
+      question:'What is currency of Bangladesh?',
+      options:[
+        {
+          text:'INR',
+          status:false
+        },
+        {
+          text:'BDT',
+          status:true
+        },
+        {
+          text:'USD',
+          status:false
+        },
+        {
+          text:'MDT',
+          status:false
+        },
+      ]
+    },
+  ]
+
+  let userAnswers = reactive([
+    {}
+  ])
+  let totalAnsweredQuestions = ref(0)
+  let currentQuestion = ref(0)
+  let progress = ref(0)
+
+  const submit = function(){
+    currentQuestion++
+    totalAnsweredQuestions++
+    progress = `(questions.length * totalAnsweredQuestions)/100 %`
+  }
+
+  const finalSubmit = function(){
+
+  }
+</script>
+
+<template>
+  <Question/>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/Question.vue
@@ -0,0 +1,37 @@
+<script setup>
+
+</script>
+
+<template>
+  
+  <div class="col col-md-6 offset-md-3">
+    <div class="card">
+      <div class="card-header">
+        <h4 class="text-center">Vue Quiz</h4>
+      </div>
+      <div class="card-body">
+        <h5 class="card-title"><strong>{{ questions[currentQuestion].question }} </strong></h5>
+        <div class="form-check" v-for="(option,index) in questions[currentQuestion].options" :key="index">
+          <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+          <label class="form-check-label" for="flexRadioDefault1">
+            {{ option }}
+          </label>
+        </div>
+        <div class="col col-md-4 offset-md-4">
+          <a href="#" class="btn btn-primary btn-sm mt-3" @click="submit">Submit Answer</a>
+        </div>
+      </div>
+      <div class="card-footer text-muted">
+        0/0 Questions answered
+        <div class="progress">
+          <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" :style="progress"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  
+</template>
+
+<style scoped>
+</style>
